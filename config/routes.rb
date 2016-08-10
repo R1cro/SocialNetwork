e2c66d97319df8646eade7b64959e9fe5e773a1f
R1cro <teseyko@outlook.com>
2016-08-10 19:49:29 +0300

    First pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get 'landing_pages/help'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'landing_pages#home'
 end
