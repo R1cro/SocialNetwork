69c0ecf327ae40f62549448aee151754c57d04f2
R1cro <teseyko@outlook.com>
2016-08-11 19:04:05 +0300

    Heroku fonts fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  before_save { self.email = email.downcase }
+  before_save { email.downcase! }
   validates :nickname, presence: true, length: { maximum: 15 }
   validates :email, presence: true, length: { maximum: 40 },
                                     format: { with: VALID_EMAIL_REGEX },
                                     uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password, presence:  true, length: { minimum: 6}
+  validates :password, presence:  true, length: { minimum: 6 }
 end
