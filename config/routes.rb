0edcef64d29daa1bc85ba0e675f99dcaac360594
R1cro <teseyko@outlook.com>
2016-08-11 19:37:30 +0300

    SignUp

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
   get  '/contact', to: 'landing_pages#contact'
   get  '/signup',  to: 'users#new'
 
+  resources :users
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'landing_pages#home'
 end
