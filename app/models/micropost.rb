bcae4b03fd8ee8ca6aa3c08adfc344efc1ffcd0b
Siarhei Padlozny <barmidrol@gmail.com>
2016-09-26 19:04:53 +0300

    Add newline for every error message

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,5 +2,5 @@ class Micropost < ApplicationRecord
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
-  validates :content, presence: true, length: { maximum: 200 }, allow_nil: false
+  validates :content, presence: true, length: { minimum: 3, maximum: 200 }, allow_nil: false
 end
