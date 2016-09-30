848ec3e5cdc84482f6459b5cd234e771fd14f3f8
R1cro <teseyko@outlook.com>
2016-09-30 21:06:00 +0300

    Change main text font for web-application. Add some improvements for views.

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,25 +13,32 @@ User.create!(email: "ateseyko@qulix.com",
              activated_at: Time.zone.now,
              admin: true)
 
+User.create!(email: "teseyko@outlook.com",
+             password:              "swseuz2U",
+             password_confirmation: "swseuz2U",
+             activated: true,
+             activated_at: Time.zone.now,
+             admin: true)
+
 User.create!(email: "r1cro@qulix.com",
             password:              "swseuz2U",
             password_confirmation: "swseuz2U",
             activated: true,
             activated_at: Time.zone.now,
-            admin: true)
+            admin: false)
 User.create!(email: "bill@qulix.com",
              password:              "swseuz2U",
              password_confirmation: "swseuz2U",
              activated: true,
              activated_at: Time.zone.now,
-             admin: true)
-User.create!(email: "www@qulix.com",
+             admin: false)
+User.create!(email: "johne@qulix.com",
              password:              "swseuz2U",
              password_confirmation: "swseuz2U",
              activated: true,
              activated_at: Time.zone.now,
-             admin: true)
-10.times do |n|
+             admin: false)
+530.times do |n|
   email = "user-#{n+1}@mail.com"
   password = "12345678"
   User.create!(email: email,
@@ -41,15 +48,15 @@ User.create!(email: "www@qulix.com",
                activated_at: Time.zone.now)
 end
 
-users = User.order(:created_at).take(10)
-10.times do
-  content = Faker::Lorem.sentence(8)
+users = User.order(:created_at).take(530)
+30.times do
+  content = Faker::Lorem.sentence(35)
   users.each { |user| user.microposts.create!(content: content) }
 end
 
 users = User.all
 user  = users.first
-following = users[2..10]
-followers = users[3..6]
+following = users[2..1000]
+followers = users[300..600]
 following.each { |followed| user.follow(followed) }
 followers.each { |follower| follower.follow(user) }
