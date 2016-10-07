ff75945feb655c4d454f9c9aa6efbbfffc5d419d
R1cro <teseyko@outlook.com>
2016-10-07 16:32:35 +0300

    Views and models fixes

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -2,7 +2,9 @@ class UserProfile < ApplicationRecord
   belongs_to :user, inverse_of: :user_profile
 
   def age(birthday)
-    (Time.now.to_s(:number).to_i - birthday.to_time.to_s(:number).to_i)/10e9.to_i
+    if birthday.present?
+      (Time.now.to_s(:number).to_i - birthday.to_time.to_s(:number).to_i)/10e9.to_i
+    end
   end
 end
 
