e57a202047da42d8c8497266ebac1e3a7ee3ca25
R1cro <teseyko@outlook.com>
2016-10-07 15:50:56 +0300

    Hot fixes

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -1,5 +1,5 @@
 class UserProfile < ApplicationRecord
-  belongs_to :user, optional: :city
+  belongs_to :user, inverse_of: :user_profile
 
   def age(birthday)
     (Time.now.to_s(:number).to_i - birthday.to_time.to_s(:number).to_i)/10e9.to_i
