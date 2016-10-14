d13362e6da43e5f75ce51e085df56808d796be20
R1cro <teseyko@outlook.com>
2016-10-14 18:31:21 +0300

    Faker for user and post create

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,7 @@
 params =  {
-  me:
-    {
-      email: "ateseyko@qulix.com",
-      password:              "swseuz2U",
-      password_confirmation: "swseuz2U",
-      activated: true,
-      activated_at: Time.zone.now,
-      admin: true,
-      user_profile_attributes:  {
-        first_name: "Anatoly",
-        second_name: "Teseyko"
-      }
-    },
-  user_1: {
-    email: "r1cro@qulix.com",
+admin:
+  {
+    email: "ateseyko@qulix.com",
     password:              "swseuz2U",
     password_confirmation: "swseuz2U",
     activated: true,
@@ -23,34 +11,41 @@ params =  {
       first_name: "Anatoly",
       second_name: "Teseyko"
     }
-  },
-  user_2: {
-    email: "macbeth3@qulix.com",
-    password:              "swseuz2U",
-    password_confirmation: "swseuz2U",
-    activated: true,
-    activated_at: Time.zone.now,
-    admin: true,
-    user_profile_attributes:  {
-      first_name: "Macbeth",
-      second_name: "III"
-    }
   }
 }
 
-User.create!(params[:me])
-User.create!(params[:user_1])
-User.create!(params[:user_2])
+User.create!(params[:admin])
 
-users = User.order(:created_at).take(3)
-12.times do
-  content = '#tag ' + Faker::Lorem.sentence(12)
+50.times do |n|
+  first_name  = Faker::Name.first_name
+  second_name  = Faker::Name.last_name
+  email = "user.#{n+1}@social.net"
+  password = "12345678"
+  User.create!(
+               email: email,
+               password:              password,
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now,
+               admin: true,
+               user_profile_attributes:  {
+                 first_name: first_name,
+                 second_name: second_name
+               }
+  )
+end
+
+users = User.order(:created_at).take(50)
+
+20.times do
+  #tag_name = Faker::Lorem.word
+  content = Faker::Lorem.sentence(13)
   users.each { |user| user.microposts.create!(content: content) }
 end
 
 users = User.all
 user  = users.first
-following = users[2..3]
-followers = users[1..3]
+following = users[2..50]
+followers = users[15..30]
 following.each { |followed| user.follow(followed) }
 followers.each { |follower| follower.follow(user) }
