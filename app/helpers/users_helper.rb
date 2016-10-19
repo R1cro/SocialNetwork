fb5fc787d105b9c4951d4853f588e6232dbb54f1
R1cro <teseyko@outlook.com>
2016-10-19 19:57:49 +0300

    Functional and style improvements. Bug fixes.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,7 +3,7 @@ module UsersHelper
     if user.user_profile.avatar.url.present?
       avatar_url = user.user_profile.avatar.url
     else
-      avatar_url = 'http://res.cloudinary.com/r1cro-socialnetwork/image/upload/v1476808829/defaultavatar_oqsdpi.png'
+      avatar_url = 'http://res.cloudinary.com/r1cro-socialnetwork/image/upload/v1476871369/defaultavatar_blue_msvxkv.jpg'
     end
     image_tag(avatar_url, alt: user.email, class: "avatar", width: size)
   end
--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -10,11 +10,11 @@ class UserProfile < ApplicationRecord
   end
 
   private
-
   def avatar_size
     if avatar.size > 5.megabytes
       errors.add(:avatar, "should be less than 5MB")
     end
   end
+
 end
 
