3106ddab2b91da0e58fed3c3f7e8656e57abce56
R1cro <teseyko@outlook.com>
2016-10-20 14:55:19 +0300

    Indentation and minor bugs fix.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,12 @@
 module UsersHelper
+  DEFAULT_AVATAR = 'http://res.cloudinary.com/r1cro-socialnetwork/image/upload/v1476871369/defaultavatar_blue_msvxkv.jpg'
+
   def avatar_for(user, size)
-    if user.user_profile.avatar.url.present?
-      avatar_url = user.user_profile.avatar.url
-    else
-      avatar_url = 'http://res.cloudinary.com/r1cro-socialnetwork/image/upload/v1476871369/defaultavatar_blue_msvxkv.jpg'
-    end
+    avatar_url = if user.user_profile.avatar.url.present?
+                    user.user_profile.avatar.url
+                 else
+                    DEFAULT_AVATAR
+                 end
     image_tag(avatar_url, alt: user.email, class: "avatar", width: size)
   end
 end
