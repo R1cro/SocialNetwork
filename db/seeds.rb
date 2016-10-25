aafaaf5278473a996844bd55a0bdae6d50bc1d83
R1cro <teseyko@outlook.com>
2016-10-25 16:15:23 +0300

    Minor changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,17 @@
 params =  {
-  admin:
-    {
-      email: "ateseyko@qulix.com",
-      password:              "12345678",
-      password_confirmation: "12345678",
-      activated: true,
-      activated_at: Time.zone.now,
-      admin: true,
-      user_profile_attributes:  {
-        first_name: "Anatoly",
-        second_name: "Teseyko"
-      }
+  admin: {
+    email: "ateseyko@qulix.com",
+    password:              "12345678",
+    password_confirmation: "12345678",
+    activated: true,
+    activated_at: Time.zone.now,
+    admin: true,
+    user_profile_attributes: {
+      first_name: "Anatoly",
+      second_name: "Teseyko",
+      remote_avatar_url: "http://res.cloudinary.com/r1cro-socialnetwork/image/upload/v1476808829/defaultavatar_oqsdpi.png"
     }
+  }
 }
 
 User.create!(params[:admin])
@@ -21,16 +21,19 @@ User.create!(params[:admin])
   second_name  = Faker::Name.last_name
   email = Faker::Internet.free_email
   password = "12345678"
+  get_person = Net::HTTP.get(URI('https://randomuser.me/api/'))
+  avatar_url = JSON.parse(get_person)['results'][0]['picture']['large']
   User.create!(
     email: email,
-    password: password,
+    password:              password,
     password_confirmation: password,
     activated: true,
     activated_at: Time.zone.now,
     admin: true,
     user_profile_attributes:  {
       first_name: first_name,
-      second_name: second_name
+      second_name: second_name,
+      remote_avatar_url: avatar_url
     }
   )
 end
