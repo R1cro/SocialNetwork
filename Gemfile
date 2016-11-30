614598ed7727a5b5c7086f9875d6ba1371d3b20f
R1cro <teseyko@outlook.com>
2016-11-30 17:18:34 +0300

    'Drag-n-Drop' functionality added to micropost form (only upload to Cloudinary).

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ gem 'acts-as-taggable-on'
 gem 'momentjs-rails', '~> 2.15', '>= 2.15.1'
 gem 'bootstrap3-datetimepicker-rails', '~> 4.17', '>= 4.17.42'
 gem 'bootstrap-datepicker-rails', '~> 1.6', '>= 1.6.0.1'
+gem 'dropzonejs-rails'
 
 group :development, :test do
   gem 'byebug',  '9.0.0', platform: :mri
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,9 +17,11 @@
 //= require bootstrap
 //= require moment
 //= require bootstrap-datetimepicker
+//= require dropzone
 //= require_tree .
 
 document.addEventListener("turbolinks:load", function() {
+
     $('.datepicker').datetimepicker({
         format: "DD/MM/YYYY",
         icons: {
@@ -30,10 +32,10 @@ document.addEventListener("turbolinks:load", function() {
 
    $("div[id|='reply-to-micropost']").hide()
    window.toggleReply = function (id) {
-        $("#reply-to-micropost-" + id.toString()).slideToggle(500);
-    }
+       $("#reply-to-micropost-" + id.toString()).slideToggle(500);
+   }
 
-    if ($('#scrolling').size() > 0) {
+   if ($('#scrolling').size() > 0) {
         $(window).on('scroll', function() {
             var url;
             url = $('.pagination .next_page a').attr('href');
@@ -43,6 +45,72 @@ document.addEventListener("turbolinks:load", function() {
                 $.getScript(url);
             }
         });
-        return;
     }
+
+    Dropzone.autoDiscover = false;
+    var imageDropzone = new Dropzone(document.getElementById('dropzone-area'), {
+        url: 'https://api.cloudinary.com/v1_1/r1cro-socialnetwork/image/upload',
+        maxFiles: 3,
+        parallelUploads: 3,
+        maxFilesize: 2, //MB
+        addRemoveLinks: true,
+        autoProcessQueue: false,
+        uploadMultiple: false,
+        acceptedFiles:'.jpg,.png,.jpeg,.gif',
+        accept: function(file, done) {
+            console.log("Uploaded");
+            done();
+        },
+        init: function() {
+            this.on("maxfilesexceeded", function(file) {
+                this.removeFile(file);
+            });
+        }
+    });
+
+    imageDropzone.on('drop', function(drop) {
+        drop.stopPropagation();
+        drop.preventDefault();
+        var imageUrl = drop.dataTransfer.getData('URL');
+        if(imageUrl.length > 0) {  // If URL - ignore
+            $('#dropzone-area').removeClass('well');
+            document.getElementById('dropzone-upload').style.display = 'none';
+        }
+        else {
+            document.getElementById('dropzone-upload').style.display = 'block';
+        }
+    });
+
+    $('#dropzone-textarea').bind('dragover', function () {
+        $('#dropzone-area').addClass('well');
+    });
+
+    $('#dropzone-textarea').bind('dragleave', function() {
+        $('#dropzone-area').removeClass('well');
+    });
+
+    $('#dropzone-upload').on("click", function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+        if (imageDropzone.getQueuedFiles().length > 0) {
+            imageDropzone.processQueue();
+        } else {
+            imageDropzone.uploadFiles([]);
+            document.getElementById('dropzone-upload').style.display='none';
+        }
+        document.getElementById('dropzone-upload').style.display='none';
+    });
+
+    imageDropzone.on('sending', function (file, xhr, formData) {
+        formData.append('api_key', 419667145398731);
+        formData.append('timestamp', Date.now() / 1000 | 0);
+        formData.append('upload_preset', 'r1cro-images');
+    });
+
+    imageDropzone.on('success', function (file, response) {
+        console.log('Success! Cloudinary public ID is', response.public_id);
+        imageDropzone.removeAllFiles();
+        $('#dropzone-area').removeClass('well');
+    });
+
 })
